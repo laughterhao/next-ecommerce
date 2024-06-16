723414b35e4340b4e1acbdedd58280e0eb9d74d9
laughterhao <laughterhao@gmail.com>
2024-06-16 16:11:26 +0800

    會員登入測試

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,131 @@
-import React from 'react'
+"use client";
+
+enum MODE {
+  LOGIN = "LOGIN",
+  REGISTER = "REGISTER",
+  RESET_PASSWORD = "RESET_PASSWORD",
+  EMAIL_VERIFICATION = "EMAIL_VERIFICATION",
+}
+
+import { useWixClinet } from "@/hook/useWixClient";
+import React, { useState } from "react";
 
 export default function LoginPage() {
+  const [mode, setMode] = useState(MODE.LOGIN);
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailCode, setEmailCode] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [message, setmessage] = useState("");
+
+  const fornTitle =
+    mode === MODE.LOGIN
+      ? "Log in"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Rest Your Password"
+      : "Verify Your Email";
+
+  const buttonTitle =
+    mode === MODE.LOGIN
+      ? "Log"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Rest"
+      : "Verify";
+  const wixClient = useWixClinet();
   return (
-    <div>LoginPage</div>
-  )
+    <div className="h-[calc(100vh-80px)] px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 flex items-center justify-center">
+      <form action="" className="flex flex-col gap-8">
+        <h1 className="text-2xl font-semibold">{fornTitle}</h1>
+        {mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+            <label htmlFor="" className="text-sm text-gray-700">
+              UserName
+            </label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Nic"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        ) : null}
+        {mode !== MODE.EMAIL_VERIFICATION ? (
+          <div className="flex flex-col gap-2">
+            <label htmlFor="" className="text-sm text-gray-700">
+              E-mail
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Nic@gmil.com"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            <label htmlFor="" className="text-sm text-gray-700">
+              E-mail
+            </label>
+            <input
+              type="text"
+              name="emailCode"
+              placeholder="Code"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        )}
+        {mode === MODE.LOGIN || mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+            <label htmlFor="" className="text-sm text-gray-700">
+              PassWord
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        ) : null}
+        {mode === MODE.LOGIN && (
+          <div
+            className="text-sm underline cursor-pointer"
+            onClick={() => setMode(MODE.RESET_PASSWORD)}
+          >
+            Forgot PassWord?
+          </div>
+        )}
+        <button
+          className="bg-lama text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed "
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading.." : buttonTitle}
+        </button>
+        {error && <div className="text-red-600">{error}</div>}
+        {mode === MODE.LOGIN && (
+          <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.REGISTER)}>
+            {"Don't"} have an account?
+          </div>
+        )}
+        {mode === MODE.REGISTER && (
+          <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.LOGIN)}>
+            Have an account?
+          </div>
+        )}
+        {mode === MODE.RESET_PASSWORD && (
+          <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.LOGIN)}>
+            Go back to Login
+          </div>
+        )}
+        {message && <div className="text-green-600 text-sm">{message}</div>}
+      </form>
+    </div>
+  );
 }
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import CartModal from "./CartModal";
+import { useWixClinet } from "@/hook/useWixClient";
 
 export default function NavIcons() {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -19,6 +20,19 @@ export default function NavIcons() {
     }
     setIsProfileOpen((prev) => !prev);
   };
+  //設定 Wix連線
+  const wixClient = useWixClinet()
+  const login =async()=>{
+    const loginRequestData = wixClient.auth.generateOAuthData(
+      "http://localhost:3000",
+      "https://vercel.com/",
+    );
+    console.log(loginRequestData)
+
+    localStorage.setItem("oAuthRedirectData", JSON.stringify(loginRequestData))
+    const {authUrl} = await wixClient.auth.getAuthUrl(loginRequestData)
+    window.location.href = authUrl
+  }
   return (
     <div className="flex items-center gap-4 xl:gap-6 relative">
       <Image
@@ -26,7 +40,8 @@ export default function NavIcons() {
         alt=""
         width={22}
         height={22}
-        onClick={handleProfile}
+        // onClick={handleProfile}
+        onClick={login}
       />
       {isProfileOpen && (
         <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,.2)] z-20">
